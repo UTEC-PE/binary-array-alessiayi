1b95de89796202ae7363ca3d529a600ae659f0d8
Alessia Yi <alessia.yi@utec.edu.pe>
2018-10-30 21:43:29 -0500

    H

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
+#include "binary.h"
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    cout << "===========================================================" << endl;
-    cout << "\Binary Array Practice" << endl;
-    cout << "===========================================================" << endl << endl;
+  cout << "===========================================================" << endl;
+  cout << "Binary Array Practice" << endl;
+  cout << "===========================================================" << endl << endl;
 
-    system("read");
-    return EXIT_SUCCESS;
+  boolean arr1(8);
+  arr1.bitOn(7);
+  cout << arr1[7];
+
+  return EXIT_SUCCESS;
 }
-
